0c9bd53aeeb47d1cd3c03223403e6f90eb05af53
Satyam Lal <ssatyamlal@gmail.com>
2026-04-10 02:16:26 +0530

    create a constructor for Config

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,26 @@
 use std::env;
 use std::fs;
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
-
-    println!("Seaching for {}", config.query);
-    println!("In file {}", config.file_path);
-
-    let contents = fs::read_to_string(config.file_path).expect("Unable to read the file!");
-    println!("With text:\n{contents}");
-}
 
 struct Config {
     query: String,
     file_path: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Config { query, file_path }
+    }
+}
 
-    Config { query, file_path }
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args);
+
+    println!("Seaching for the word: '{}'", config.query);
+    println!("In file: '{}'", config.file_path);
+
+    let contents = fs::read_to_string(config.file_path).expect("Unable to read the file!");
+    println!("With text:\n{contents}");
 }
