1a5b95ac1313324e59fe22b8f748db6c5022083c
Satyam Lal <ssatyamlal@gmail.com>
2026-04-10 14:22:51 +0530

    1. Change Config::new() -> Config::build() 2. handle error with unwrap_or_else()

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use std::env;
-use std::fs;
+use std::{env, fs, process};
 
 struct Config {
     query: String,
@@ -7,16 +6,22 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments!");
+        }
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Error while parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Seaching for the word: '{}'", config.query);
     println!("In file: '{}'", config.file_path);
